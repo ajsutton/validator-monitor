d0590bca6daec216fe5dc8b8b037c02bccdedf6e
Adrian Sutton <adrian.sutton@consensys.net>
2021-09-06 19:17:31 +1000

    Tweak JSON logging.

--- src/reporting/logReporter.js
+++ src/reporting/logReporter.js
@@ -1,10 +1,11 @@
 
 module.exports = {
-    report(problem, block, moreInfo) {
-        console.log({
+    report(problem, block, problemObject) {
+        console.log(JSON.stringify({
             problem, 
             validatorId: block.proposer_index, 
-            slot: block.slot
-        });
+            slot: block.slot,
+            problemObject
+        }));
     }
 }
