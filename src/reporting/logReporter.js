502746d6ec4573f7ab629d144efc5b95991086bc
Adrian Sutton <adrian.sutton@consensys.net>
2021-09-09 23:55:39 +1000

    Include graffiti in log.

--- src/reporting/logReporter.js
+++ src/reporting/logReporter.js
@@ -1,10 +1,22 @@
 
+function hex2a(hex) {
+    var str = '';
+    for (var i = 2; i < hex.length; i += 2) {
+        var v = parseInt(hex.substr(i, 2), 16);
+        if (v) {
+            str += String.fromCharCode(v);
+        }
+    }
+    return str;
+}  
+
 module.exports = {
     report(problem, block, problemObject) {
         console.log(JSON.stringify({
             problem, 
             validatorId: block.proposer_index, 
             slot: block.slot,
+            graffiti: hex2a(block.body.graffiti),
             problemObject
         }));
     }
